a1a23b0a989a19dd0c5c79bd8aa86df2e3bf511d
Administrator <Administrator@plmdev.digiteki.com>
2024-04-23 16:19:12 +0900

    dd

--- Algorithm/src/NumberSum/Main.java
+++ Algorithm/src/NumberSum/Main.java
@@ -3,32 +3,12 @@ package NumberSum;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		StringBuilder sb = new StringBuilder();
-		
-		sb.append(st.nextToken());
-
-		String a = "c=";
-		String b = "c-";
-		String c = "dz=";
-		String d = "d-";
-		String e = "lj";
-		String f = "nj";
-		String g = "s=";
-		String h = "z=";
-		
-		int count = 0;
-		
-		
-		
+		String a = br.readLine();
 	}
 }
