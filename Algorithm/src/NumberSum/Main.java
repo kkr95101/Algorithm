81e35cea80629e118997a95184c87992334c8c0e
Administrator <Administrator@plmdev.digiteki.com>
2024-04-29 16:08:32 +0900

    dd

--- Algorithm/src/NumberSum/Main.java
+++ Algorithm/src/NumberSum/Main.java
@@ -10,23 +10,6 @@ public class Main {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
-		String number = br.readLine();
-		for (int i = 0; i < Integer.valueOf(number); i++) {
-			String input = br.readLine();
-			int amount = Integer.parseInt(input);
-
-			int quarters = amount / 25;
-			amount %= 25;
-
-			int dimes = amount / 10;
-			amount %= 10;
-
-			int nickels = amount / 5;
-			amount %= 5;
-
-			int pennies = amount;
-
-			System.out.println(quarters+" "+dimes+" "+nickels+" "+pennies);
-		}
+		
 	}
 }
