fe99b1665678ff26e3628d6c922e881169112f2f
samsung <samsung@LAPTOP-T2R4K3R0>
2024-05-01 10:09:37 +0900

    dd

--- Algorithm/src/NumberSum/Main.java
+++ Algorithm/src/NumberSum/Main.java
@@ -3,6 +3,8 @@ package NumberSum;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,22 +15,18 @@ public class Main {
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		StringBuilder sb = new StringBuilder();
-		
-		sb.append(st.nextToken());
+		String a =(String)st.nextElement();
 
-		String a = "c=";
-		String b = "c-";
-		String c = "dz=";
-		String d = "d-";
-		String e = "lj";
-		String f = "nj";
-		String g = "s=";
-		String h = "z=";
-		
-		int count = 0;
-		
-		
-		
+		for (int i = 0; i < a.length(); i++) {
+			a= a.replace("c=", "0");
+			a= a.replace("c-", "1");
+			a= a.replace("dz=","2");
+			a= a.replace("d-", "3");
+			a= a.replace("lj", "4");
+			a= a.replace("nj", "5");
+			a= a.replace("s=", "6");
+			a= a.replace("z=", "7");
+		}
+		System.out.println(a.length());
 	}
 }
