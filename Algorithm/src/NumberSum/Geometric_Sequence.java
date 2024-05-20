cca5e62446bd12364ce6597cc65a491c94e46022
Administrator <Administrator@plmdev.digiteki.com>
2024-05-20 09:26:56 +0900

    convert uppercase

--- Algorithm/src/NumberSum/Geometric_Sequence.java
+++ Algorithm/src/NumberSum/Geometric_Sequence.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class geometricSequence {
+public class Geometric_Sequence {
 
 	public static void main(String[] args) throws IOException {
 
