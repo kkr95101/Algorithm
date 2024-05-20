e45fb4a7eba09c5dfded680ffac3d80b054a366f
Administrator <Administrator@plmdev.digiteki.com>
2024-05-20 11:50:10 +0900

    aaa

--- Algorithm/src/NumberSum/Decimal.java
+++ Algorithm/src/NumberSum/Decimal.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Decimal {
 
     public static void main(String[] args) throws IOException {
 
