a476a803bcefa0a8bdaba606ce3618db1d9ef7a1
Administrator <Administrator@plmdev.digiteki.com>
2024-07-05 17:23:29 +0900

    dfsd

--- Algorithm/src/NumberSum/Main.java
+++ Algorithm/src/NumberSum/Main.java
@@ -1,43 +1,44 @@
 package NumberSum;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.valueOf(st.nextToken());
-		int M = Integer.valueOf(st.nextToken());
-		
-		Map<String, Integer> nameArray = new HashMap<>();
-		for (int i = 0; i < N+M; i++) {
-			String input= br.readLine();
-			if(!(nameArray.get(input)==null)) {
-				nameArray.put(input, 2);
-			}else {
-				nameArray.put(input, 1);
-			}
-		}
-		StringBuilder sb = new StringBuilder(); 
-		int a = 0;
-		for (Map.Entry<String,Integer> entry : nameArray.entrySet()) {
-			if(entry.getValue()==2) {
-				a++;
-				sb.append(entry.getKey()).append("/br");
-			}
-		}
-		System.out.println(a);
-		System.out.println(sb);
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        Set<String> unheard = new HashSet<>();
+        Set<String> tempUnheard = new HashSet<>();
+        Set<String> unseen = new HashSet<>();
+        Set<String> tempUnseen = new HashSet<>();
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            unheard.add(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            unseen.add(st.nextToken());
+        }
+        tempUnheard = unheard;
+        // 듣도 보도 못한 사람의 명단을 구함 (교집합)
+        unheard.retainAll(unseen);
+        
+        int a = tempUnheard.size()-unheard.size();
+        System.out.println(a);
+        int b = unseen.size()-unheard.size();
+        System.out.println(b);
+        System.out.println(a+b);
+       
+    }
 }
